8876c529c98e83c986d41713b681ed88e22c2d66
egarrisxn <egarrisxn@gmail.com>
2025-11-19 15:41:46 -0500

    updates

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -29,9 +29,9 @@ export async function updateSession(request: NextRequest) {
     }
   );
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { data } = await supabase.auth.getClaims();
+
+  const user = data?.claims;
 
   if (
     !user &&
